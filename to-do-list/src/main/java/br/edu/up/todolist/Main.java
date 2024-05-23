2abcb7bac4b158aeca8a30f6020b943d78364eb6
Lucas Huzar <lucashuzar@outlook.com>
2024-05-23 10:05:08 -0300

    Configurações do Log

--- to-do-list/src/main/java/br/edu/up/todolist/Main.java
+++ to-do-list/src/main/java/br/edu/up/todolist/Main.java
@@ -1,8 +1,12 @@
 package br.edu.up.todolist;
 
-public class Main {
-    public static void main(String[] args) {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("Iniciando...");
         System.out.println("Hello world!");
 
     }
